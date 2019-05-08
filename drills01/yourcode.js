994c3a529ad09a593ff4f28e04a0ba9cbdd29e3e
Juliann Talkington <perrigoj@bellsouth.net>
2019-05-08 16:09:57 -0700

    drills01 complete

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,34 @@
 
-function sumArray(){
-	
+function sumArray(array){
+  var sum = 0;
+  array.forEach(element => {
+    sum += element;
+  })
+  return sum;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(array, number){
+  var sum = 0;
+  newArray = [];
+  array.forEach(element => {
+    if (sum + element < number) {
+      sum += element;
+      newArray.push(element);
+    } 
+  });
+  return newArray;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(array, number){
+  var newArray = [];
+  array.forEach(element => {
+   if (element.length < number) {
+     newArray.push(element);
+   }
+  })
+  return newArray;
 }
 
-function makeLabel(){
-
+function makeLabel(obj){
+  return obj.greeting + ' ' + obj.givenName +' ' + obj.familyName + '\n' + obj[ 'home address' ].streetNumber + ' ' + obj[ 'home address' ].streetName + '\n' + obj[ 'home address' ].city + ', ' +  obj[ 'home address' ].state  + ' ' + obj[ 'home address' ].zip;
 }
