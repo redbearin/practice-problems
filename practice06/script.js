30628439cc374cabe36aabe0a5b7a1fed782748c
Juliann Talkington <perrigoj@bellsouth.net>
2019-05-11 17:12:01 -0700

    practice06 complete

---
+++ practice06/script.js
@@ -0,0 +1,21 @@
+// ## Numeric Spirals
+
+// Build a function that takes one parameter and prints out the Fibonacci sequence based on the input parameter. 
+
+// - Write a function that takes one parameter
+// 	- Parameter 1 - A number
+// - The function should output the Fibonacci sequence to the number of spaces set by parameter 1 starting from zero.
+// - Example:
+// 	- `fibSequence(12);`
+// 	- Output - `0 1 1 2 3 5 8 13 21 34 55 89`
+// - If you are unfamiliar with the Fibonacci sequence <a href="https://www.google.com" target="_blank">Click Here</a>
+
+function fibSequence(number){
+  var arr = [0, 1];
+  for (var i = 1; i < number - 1; i++){
+    arr[i+1] = arr[i-1] + arr[i];
+  }
+  return arr.join(' ');
+}
+
+fibSequence(12);
