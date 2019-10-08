3dab57d022eba375ccdbcb06a6d1ae3974825a15
Juliann <perrigoj@bellsouth.net>
2019-10-08 16:01:24 +0000

    practice 14 complete

---
+++ practice14/script.js
@@ -0,0 +1,18 @@
+function arrayFactors(array) {
+  var object = {};
+  let uniqueArray = [...new Set(array)];
+  console.log (uniqueArray);
+  for(var index = 0; index < uniqueArray.length; index++) {
+    var tempArray = [];
+    for(var index2 = 0; index2 < uniqueArray.length; index2++) {
+      if (index2 !== index && uniqueArray[index] % uniqueArray[index2] === 0) {
+        tempArray.push(uniqueArray[index2]);
+      }
+      console.log(tempArray);
+    }
+    object[uniqueArray[index]] = tempArray;
+  }
+  return object;
+}
+
+arrayFactors([4,2,8,6,3,9,4]);
