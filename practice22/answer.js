e606a3f70361c99b867cb1583a39b830d4d15679
Juliann <perrigoj@bellsouth.net>
2019-10-27 17:05:48 +0000

    practice 22 complete

--- practice22/answer.js
+++ practice22/answer.js
@@ -1,1 +1,13 @@
-answer.js
+function sortArrayOfObjectsByKey(input_array, sort_field) {
+  return input_array.sort((a,b) => 
+    (a[sort_field] > b[sort_field]) ? 1 : -1);
+}
+
+const input_array = [
+	{name: 'John', age:42, gender:'M'},
+	{name: 'Jack', age:34, gender:'M'},
+	{name: 'Michael',age:40, gender: 'M'},
+	{name: 'Kendra',age:48, gender:'F'}
+]
+
+sortArrayOfObjectsByKey(input_array, 'name');
