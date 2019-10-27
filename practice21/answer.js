dcde335bedf5f31cda1715404bd479afd19fc32a
Juliann <perrigoj@bellsouth.net>
2019-10-27 03:47:12 +0000

    practice 21 complete

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,4 @@
-answer.js
+for(var numberUp = 1, numberDown = -1; numberUp < 10, numberDown > -10; numberUp++, numberDown--) {
+  console.log('number up: ', numberUp);
+  console.log('number down: ', numberDown);
+}
