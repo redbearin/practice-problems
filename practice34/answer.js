3eef3d4002d58e3d996be6c1f39dd9e786938e24
Juliann <perrigoj@bellsouth.net>
2019-11-10 03:43:26 +0000

    practice34 complete

---
+++ practice34/answer.js
@@ -0,0 +1,16 @@
+function longestCommonString(str1, str2) {
+  var commonString = '';
+  for(var str1Index = 0; str1Index < str1.length; str1Index++){
+    for(var str2Index = 0; str2Index < str2.length; str2Index++){   
+      if (str1[str1Index] === str2[str2Index]){
+        commonString += str2[str2Index];
+        break;
+      }
+    }
+  }
+  return commonString;
+}
+
+var str1 = 'abcdefg';
+var str2 = 'abc';
+longestCommonString(str1, str2);
